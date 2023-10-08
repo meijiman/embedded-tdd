e6eeb05451566c6a2670b15313035812ac938432
MeijiMan <meijiman@outlook.com>
2023-10-08 12:40:49 +0700

    add some more tests and implementations

--- src/LedDriver.c
+++ src/LedDriver.c
@@ -3,15 +3,25 @@
 
 static uint16_t * ledsAddress;
 
+enum {
+	ALL_LEDS_ON = ~0,
+	ALL_LEDS_OFF = ~ALL_LEDS_ON
+};	
+
 void LedDriver_Create(uint16_t * address) {
 	ledsAddress = address;
-	*ledsAddress = 0;
+	*ledsAddress = ALL_LEDS_OFF;
 }
 
 void LedDriver_TurnOn(int ledNumber) {
-	*ledsAddress = 1;	
+	*ledsAddress |= 1 << (ledNumber - 1);	
 }
 
 void LedDriver_TurnOff(int ledNumber) {
-	*ledsAddress = 0;
+	*ledsAddress &= ~(1 << (ledNumber - 1));
 }
+
+void LedDriver_TurnAllOn(void) {
+	*ledsAddress = ALL_LEDS_ON;
+}
+
--- tests/LedDriverTest.c
+++ tests/LedDriverTest.c
@@ -21,12 +21,32 @@ void test_turnOffLedOne() {
 	TEST_ASSERT_EQUAL_HEX16(0, virtualLeds);
 }
 
+void test_turnOnMultipleLeds() {
+	LedDriver_TurnOn(9);
+	LedDriver_TurnOn(8);
+	TEST_ASSERT_EQUAL_HEX16(0x180, virtualLeds);
+}
+
+void test_turnOffAnyLed() {
+	LedDriver_TurnAllOn();
+	LedDriver_TurnOff(8);
+	TEST_ASSERT_EQUAL_HEX16(0xff7f, virtualLeds);
+}
+
+void test_allOn() {
+	LedDriver_TurnAllOn();
+	TEST_ASSERT_EQUAL_HEX16(0xffff, virtualLeds);
+}
+
 int main(void)
 {
 	UNITY_BEGIN();
 	RUN_TEST(test_ledsOffAfterCreate);
 	RUN_TEST(test_turnOnLedOne);
 	RUN_TEST(test_turnOffLedOne);
+	RUN_TEST(test_turnOnMultipleLeds);
+	RUN_TEST(test_turnOffAnyLed);
+	RUN_TEST(test_allOn);
 	return UNITY_END();
 }
 
